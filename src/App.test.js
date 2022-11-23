0ec9db758d1afcae71f5f04490914ec37d0d5e27
Josephine <josephine.closan@yahoo.com>
2022-11-23 14:49:54 +0200

    change message in App.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hae/i);
+  const linkElement = screen.getByText(/Hae kohteita/i);
   expect(linkElement).toBeInTheDocument();
 });
